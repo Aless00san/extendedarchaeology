3a1f2d59214f23cfdb4310525433cd0a6f8d2167
Aless00san <mumaitronzx@gmail.com>
2023-07-20 19:16:38 +0200

    Added brushing animation

---
+++ src/main/java/net/alpaca/extendedarchaeology/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.alpaca.extendedarchaeology.item;
+
+import net.alpaca.extendedarchaeology.ExampleMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    //TODO: Do the registry of items here
+    public static final Item ANCIENT_WEAPON = registerItem("ancient_weapon", new SwordItem(ModToolMaterials.ANCIENT, 8, 3F, new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(ExampleMod.MOD_ID, name), item);
+    }
+}
